e492385c2421c72d52c2b2116358a9bbe6aeffb9
Alberto <alberto10pumas@gmail.com>
2025-12-06 17:33:14 -0600

    :coffin: Se eliminaron test innecesarios

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('smoke test', (WidgetTester tester) async {
-    // This is a basic smoke test that does nothing.
-    // It's here to make sure the test runner has at least one test to run.
-    expect(true, isTrue);
-  });
-}
